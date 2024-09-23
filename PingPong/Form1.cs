41a6909d51c9507c6bfba212ee097384586442d4
Mairen <perez.ruiz.mairen@alumnat.copernic.cat>
2024-09-23 10:54:17 +0200

    Evento KeyIsDown()
    Corrección sintaxis

--- PingPong/Form1.cs
+++ PingPong/Form1.cs
@@ -2,28 +2,29 @@ namespace PingPong
 {
     public partial class Form1 : Form
     {
+        // Velocidad de la pelota
+        int ballXSpeed = 4;
+        int ballYSpeed = 4;
+        int Speed = 2;
+
+        // Velocidad aleratoria de la pelota
+        Random rdm = new Random();
+
+        // Variables de control de movimiento
+        bool goDown, goUp;
+
+        // Puntaje y velocidad del jugador y de la IA
+        int ComputeSpeedChange = 50;
+        int Player1Score = 0;
+        int PlayerSpeed = 8;
+        int PCScore = 0;
+
+        // Configuración de las velocidades de la IA
+        int[] i = { 5, 6, 8, 9 };
+        int[] j = { 10, 9, 8, 11, 12 };
+
         public Form1()
         {
-            // Velocidad de la pelota
-            int ballXSpeed = 4;
-            int ballYSpeed = 4;
-            int Speed = 2;
-
-            // Velocidad aleratoria de la pelota
-            Random rdm = new Random();
-
-            // Variables de control de movimiento
-            bool IrAbajo , IrArriba;
-
-            // Puntaje y velocidad del jugador y de la IA
-            int ComputeSpeedChange = 50;
-            int Player1Score = 0;
-            int PlayerSpeed = 8;
-            int PCScore = 0;
-
-            // Configuración de las velocidades de la IA
-            int[] i = { 5, 6, 8, 9 };
-            int[] j = { 10, 9, 8, 11, 12 };
             InitializeComponent();
         }
 
@@ -39,12 +40,20 @@ namespace PingPong
 
         private void KeyIsDown(object sender, KeyEventArgs e)
         {
-
+            if (e.KeyCode == Keys.Down)
+                goDown = true;
+            
+            if (e.KeyCode == Keys.Up)
+                goUp = true;
         }
 
         private void KeyIsUp(object sender, KeyEventArgs e)
         {
+            if (e.KeyCode == Keys.Down)
+                goDown = false;
 
+            if (e.KeyCode == Keys.Up)
+                goUp = false;
         }
 
         private void CheckCollision(PictureBox PicOne, PictureBox PicTwo, int offset)
