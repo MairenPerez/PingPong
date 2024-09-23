1ba63b3faecec5ed147abc508d51c3b666466b52
Mairen <perez.ruiz.mairen@alumnat.copernic.cat>
2024-09-23 11:07:55 +0200

    Evento CheckCollision() y GameOVer()

--- PingPong/Form1.cs
+++ PingPong/Form1.cs
@@ -3,25 +3,25 @@ namespace PingPong
     public partial class Form1 : Form
     {
         // Velocidad de la pelota
-        int ballXSpeed = 4;
-        int ballYSpeed = 4;
-        int Speed = 2;
+        private int ballXSpeed = 4;
+        private int ballYSpeed = 4;
 
-        // Velocidad aleratoria de la pelota
-        Random rdm = new Random();
+        int ComputerSpeedChange = 50;
+        // Velocidad aleatoria de la pelota
+        private Random rdm = new Random();
 
         // Variables de control de movimiento
-        bool goDown, goUp;
+        private bool goDown;
+        private bool goUp;
 
         // Puntaje y velocidad del jugador y de la IA
-        int ComputeSpeedChange = 50;
-        int Player1Score = 0;
-        int PlayerSpeed = 8;
-        int PCScore = 0;
+        private int player1Score = 0;
+        private int playerSpeed = 8;
+        private int pcScore = 0;
 
         // Configuración de las velocidades de la IA
-        int[] i = { 5, 6, 8, 9 };
-        int[] j = { 10, 9, 8, 11, 12 };
+        private int[] i = { 5, 6, 8, 9 };
+        private int[] j = { 10, 9, 8, 11, 12 };
 
         public Form1()
         {
@@ -42,7 +42,7 @@ namespace PingPong
         {
             if (e.KeyCode == Keys.Down)
                 goDown = true;
-            
+
             if (e.KeyCode == Keys.Up)
                 goUp = true;
         }
@@ -56,14 +56,36 @@ namespace PingPong
                 goUp = false;
         }
 
-        private void CheckCollision(PictureBox PicOne, PictureBox PicTwo, int offset)
+        private void CheckCollision(PictureBox picOne, PictureBox picTwo, int offset)
         {
+            if (picOne.Bounds.IntersectsWith(picTwo.Bounds))
+            {
+                picOne.Left = offset;
 
+                int x = i[rdm.Next(i.Length)];
+                int y = j[rdm.Next(j.Length)];
+
+                if (ballXSpeed < 0)
+                    ballXSpeed = x;
+                else
+                    ballXSpeed = -x;
+
+                if (ballYSpeed < 0)
+                    ballYSpeed = -y;
+                else
+                    ballYSpeed = y;
+            }
         }
 
-        private void GameOver (string message)
+        private void GameOver(string message)
         {
-
+            Temporizador.Stop();
+            MessageBox.Show(message, "Game Over");
+            pcScore = 0;
+            player1Score = 0;
+            ballXSpeed = ballYSpeed = 4;
+            ComputerSpeedChange = 50;
+            Temporizador.Start(); // Reiniciar el juego
         }
     }
 }
